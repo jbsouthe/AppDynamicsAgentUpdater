e666999b150bb5f25064770cbfd340d396415d6d
John Southerland <jsoutherland@me.com>
2023-04-30 11:43:52 -0600

    added meta data class generation

---
+++ src/main/java-templates/com/singularity/ee/service/agentupdater/MetaData.java
@@ -0,0 +1,9 @@
+package com.singularity.ee.service.agentupdater;
+
+public class MetaData {
+    public static final String VERSION = "v${version}";
+    public static final String BUILDTIMESTAMP = "${build.time}";
+    public static final String GECOS = "John Southerland josouthe@cisco.com";
+    public static final String GITHUB = "https://github.com/jbsouthe/AppDynamicsAgentUpdater";
+    public static final String DEVNET = "https://developer.cisco.com/codeexchange/github/repo/jbsouthe/AppDynamicsAgentUpdater";
+}
